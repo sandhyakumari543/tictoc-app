d1f78e2c158f7f170627262288139bc5e75d72c9
Sandhya <sandhyakushwaha543@gmai.com>
2024-01-17 13:50:12 +0530

    change in package.json and GameBoard.js

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,21 +1,3 @@
-// import React from 'react';
-
-// const GameOver = ({winner,onRestart}) => {
-
-//   return (
-//     <>
-//       <div id="game-over">
-//         <h2>Game Over!</h2>
-//       {winner && <p>{winner} Won!</p>}
-//       {!winner && <p>It&apos;s a draw!</p>}
-//         <button onClick={onRestart}>Rematch!</button>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default GameOver;
-
 import React, { useEffect } from 'react';
 
 const GameOver = ({ winner, onRestart }) => {
